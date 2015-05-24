0c6c6a4e02234ca320b109fbb7115fef87504288
Cole Reed <ichabodcole@gmail.com>
2015-05-23 21:21:32 -0700

    rename runtime file, so as not to confuse it with a non-hacked version.

--- edge.5.0.1-hack.js
+++ edge.5.0.1-hack.js
@@ -9515,5 +9515,27 @@ AdobeEdge.yepnope = window.yepnope;// an.timeline.js - version  2.0
 /* jshint ignore:end */
 
 /* starthack */
-module.exports = AdobeEdge;
+
+// UMD loader
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return AdobeEdge;
+}));
+
 /* endhack */
